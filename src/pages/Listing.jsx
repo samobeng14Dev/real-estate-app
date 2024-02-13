4ea7dc62bfc0cb2540d562aac964caeea7735f92
Samuel Obeng <obengsamuel270@gmail.com>
2024-02-13 19:19:24 +0530

    installed swiper js and created listing page

--- src/pages/Listing.jsx
+++ src/pages/Listing.jsx
@@ -1,5 +1,28 @@
+import { useEffect, useState } from "react";
+import { useParams } from "react-router";
+import { doc, getDoc } from "firebase/firestore";
+import { Loading } from "../components";
+
 const Listing = () => {
-	return <div>Listing</div>;
+	const [listing, setListing] = useState(null);
+	const [loading, setLoading] = useState(true);
+	const params = useParams();
+	const fetchListing = async () => {
+		const docRef = doc(db, "listings", params.listingId);
+		const docSnap = await getDoc(docRef);
+		if (docSnap.exists()) {
+			setListing(docSnap.data());
+			setLoading(false);
+		}
+	};
+	useEffect(() => {
+		fetchListing();
+	}, [params.listingId]);
+
+	if (loading) {
+		return <Loading />;
+	}
+	return <div>{listing.name}</div>;
 };
 
 export default Listing;
