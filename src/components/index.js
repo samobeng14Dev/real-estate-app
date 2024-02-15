d2de574ade38da4bf59d1868fe144d95ce483b28
Samuel Obeng <obengsamuel270@gmail.com>
2024-02-15 18:12:24 +0530

    uninstalled react slick

--- src/components/index.js
+++ src/components/index.js
@@ -3,3 +3,4 @@ export { default as Navbar } from "./Navbar";
 export { default as Loading } from "./Loading";
 export { default as PrivateRoute } from "./PrivateRoute";
 export { default as ListingItem } from "./ListingItem";
+export { default as PauseOnHover } from "./PauseOnHover";
--- src/pages/Listing.jsx
+++ src/pages/Listing.jsx
@@ -2,30 +2,47 @@ import { getAuth } from "firebase/auth";
 import { doc, getDoc } from "firebase/firestore";
 import { useEffect, useState } from "react";
 import { useParams } from "react-router-dom";
-
-import { db } from "../firebase";
-
 import { Loading } from "../components";
+import { db } from "../firebase";
+import Slider from "react-slick";
 
-export default function Listing() {
+const Listing = () => {
 	const auth = getAuth();
 	const params = useParams();
 	const [listing, setListing] = useState(null);
 	const [loading, setLoading] = useState(true);
 
-	const fetchListing = async () => {
-		const docRef = doc(db, "listings", params.listingId);
-		const docSnap = await getDoc(docRef);
-		if (docSnap.exists()) {
-			setListing(docSnap.data());
-			setLoading(false);
-		}
+	var settings = {
+		dots: true,
+		infinite: true,
+		slidesToShow: 3,
+		slidesToScroll: 1,
+		autoplay: true,
+		autoplaySpeed: 2000,
+		pauseOnHover: true,
 	};
+
 	useEffect(() => {
+		async function fetchListing() {
+			const docRef = doc(db, "listings", params.listingId);
+			const docSnap = await getDoc(docRef);
+			if (docSnap.exists()) {
+				setListing(docSnap.data());
+				setLoading(false);
+			}
+		}
 		fetchListing();
 	}, [params.listingId]);
-	if (loading) {
+
+	if (loading || !listing || !listing.imgUrls) {
 		return <Loading />;
 	}
-	return <main>{listing.name}</main>;
-}
+
+	return (
+		<main>
+			<div>Slider</div>
+		</main>
+	);
+};
+
+export default Listing;
