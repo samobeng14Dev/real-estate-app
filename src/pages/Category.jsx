5c013ae89be22a262a0f365543a34e47fd8ddfa2
Samuel Obeng <obengsamuel270@gmail.com>
2024-02-27 07:01:41 +0530

    made current offer section dynamic

--- src/pages/Category.jsx
+++ src/pages/Category.jsx
@@ -118,7 +118,12 @@ const Category = () => {
 					)}
 				</>
 			) : (
-				<p>There are no current offers</p>
+				<p>
+					There are no current{" "}
+					{params.categoryName === "rent"
+						? "places for rent"
+						: "places for sale"}
+				</p>
 			)}
 		</div>
 	);
